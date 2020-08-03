e72288d178a90574d87bdc2dfbe2fcf939c41e87
Kizoky <kilerzolika@gmail.com>
2020-08-03 20:38:21 +0200

    -SimSun generator now asks for user input

--- !generate_SimSun.bat
+++ !generate_SimSun.bat
@@ -1,2 +1,4 @@
 @echo off
+echo Enter path to folder (i.e. "models\props\*.png") without "" (must be .png at the end)
+echo.
 SimSun_gen.cmd > !SimSun_Output.txt
--- SimSun_gen.cmd
+++ SimSun_gen.cmd
@@ -1,18 +1,22 @@
 @echo off
+echo Enter path to folder (i.e. "models\props\*.png") without "" (must be .png at the end)
+echo.
 setlocal EnableDelayedExpansion
 
 cd %~dp0\..
 
-for /R %%I in (models\props\*.png) do call :process "%%~I"
+set /p enteredpath=
+set "removepng=%enteredpath:*.png=%"
+
+for /R %%I in (%enteredpath%) do call :process "%%~I"
 goto :eof
 
 :process
-
 set texfullname=%~dpnx1
 set texfullname=!texfullname:*%cd%\=!
 set texfullname=%texfullname:\=/%
 
-for %%J in ("models\props\%~n1.*") do set texmaterial=%%J
+for %%J in ("%removepng%%~n1.*") do set texmaterial=%%J
 
 call :output "%texfullname%" "%texmaterial%"
 goto :eof
